c54c4eb6f332a4b95910eb41e5e595a23d8c399a
gaochao04 <824310342@qq.com>
2015-10-04 23:10:41 +0800

    prod conf

--- fis-conf.js
+++ fis-conf.js
@@ -30,10 +30,61 @@ fis.match(/^\/page\/(.*\.mod\.js)/i, {
     isMod: true
 });
 
-fis.match(/^\/public\/libs.*\.(less|css)'/i, {
-  packTo: '/public/pkg/aio.css'
-});
-
 fis.match(/^\/public\/(.*\..*)$/i, {
     release: '/public/mis/$1'
 });
+
+/**
+ * prod 产品环境配置 js、css 打包配置
+ */
+// 增加md5
+fis.media('prod').match('*.{js,css,less,png}', {
+    useHash: true
+});
+
+// js 、css 压缩配置
+fis.media('prod').match('*.js', {
+    optimizer: fis.plugin('uglify-js')
+});
+fis.media('prod').match('*.{css,less}', {
+    optimizer: fis.plugin('clean-css')
+});
+
+/**
+ * prod 环境打包配置
+ */
+fis.media('prod').match('::packager', {
+    postpackager: fis.plugin('loader')
+});
+
+fis.media('prod').match('/public/libs/third/bootstrap.css', {
+        packTo: '/public/pkg/aio.css'
+    }).media('prod').match('/public/libs/third/highlight/docco.css', {
+        packTo: '/public/pkg/aio.css'
+    }).media('prod').match('/public/libs/core/core.less', {
+        packTo: '/public/pkg/aio.css'
+    }).media('prod').match('/public/libs/third/jsmod-extend.less', {
+        packTo: '/public/pkg/aio.css'
+    });
+
+fis.media('prod').match('/public/libs/third/angular.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/jquery.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/jsmod.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/jsmod-extend.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/marked.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/highlight/highlight.pack.js', {
+      packTo: '/public/pkg/aio.js'
+    })
+    .media('prod').match('/public/libs/third/ace/src/ace.js', {
+      packTo: '/public/pkg/aio.js'
+    });
